6aa8e1fd65cbfce5307c4898f956540c4f60314c
muhammadridho-debug <idomuhammad11@gmail.com>
2026-03-10 09:09:30 +0800

    menambahkan prosedur input data berat dan tinggi badan

--- ConditionalStatement.cpp
+++ ConditionalStatement.cpp
@@ -1,2 +1,11 @@
 #include <iostream>
 using namespace std;
+
+void input(float &a, float &b)
+{
+    cout << "--- Selamat Datang Mahasiswa Kelas A 2025 ---" << endl;
+    cout << "Masukkan Berat badan (kg) : ";
+    cin >> a;
+    cout << "Masukkan Tinggi badan (meter) : ";
+    cin >> b;
+}
